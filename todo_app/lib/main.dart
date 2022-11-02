aaf26854eb7f61378c121a6ebd5156de1e95d9d4
DiemUyen <diemuyen0408@gmail.com>
2022-11-02 08:34:34 +0700

    Use Dynamic color theme

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: AppTheme.lightTheme(lightColorScheme),
+          darkTheme: AppTheme.darkTheme(darkColorScheme),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+      },
     );
   }
 }
---
+++ todo_app/lib/utils/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(ColorScheme? lightColorScheme) {
+    ColorScheme scheme = lightColorScheme ?? ColorScheme.fromSeed(seedColor: const Color(0xFFFFB057));
+    return ThemeData(
+      colorScheme: scheme,
+      useMaterial3: true
+    );
+  }
+
+  static ThemeData darkTheme(ColorScheme? darkColorScheme) {
+    ColorScheme scheme = darkColorScheme ?? ColorScheme.fromSeed(seedColor: const Color(0xFFFFB057), brightness: Brightness.dark);
+    return ThemeData(
+      colorScheme: scheme,
+      useMaterial3: true
+    );
+  }
+}
