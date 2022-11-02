48c1fe65e4d279bf3b44e642a5588d0a46c2a8d2
DiemUyen <diemuyen0408@gmail.com>
2022-11-02 09:54:03 +0700

    Create routes

---
+++ todo_app/lib/routes.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/routes/detail/detail_task_page.dart';
+import 'package:todo_app/routes/detail/notifications_setting_page.dart';
+import 'package:todo_app/routes/detail/profile_page.dart';
+import 'package:todo_app/routes/home/adding_task_bottom_sheet.dart';
+import 'package:todo_app/routes/home/notifications_page.dart';
+import 'package:todo_app/routes/home/personal_page.dart';
+import 'package:todo_app/routes/home/tasks_page.dart';
+
+class RouteGenerator {
+  // Tasks
+  static const String tasksPage = '/tasks';
+  static const String detailTaskPage = '/detailTask';
+
+  // Add task
+  static const String addingTaskBottomSheet = '/addingTask';
+
+  // Personal
+  static const String personalPage = '/personal';
+  static const String profilePage = '/profile';
+  static const String notificationsSettingPage = '/notificationsSetting';
+
+  // Notification
+  static const String notificationsPage = '/notifications';
+
+  // Private constructor
+  RouteGenerator._();
+
+  // Generate route
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case tasksPage:
+        return MaterialPageRoute(
+          builder: (_) => const TasksPage()
+        );
+      case detailTaskPage:
+        return MaterialPageRoute(
+          builder: (_) => const DetailTaskPage()
+        );
+      case addingTaskBottomSheet:
+        return MaterialPageRoute(
+          builder: (_) => const AddingTaskBottomSheet()
+        );
+      case personalPage:
+        return MaterialPageRoute(
+          builder: (_) => const PersonalPage()
+        );
+      case profilePage:
+        return MaterialPageRoute(
+          builder: (_) => const ProfilePage()
+        );
+      case notificationsSettingPage:
+        return MaterialPageRoute(
+          builder: (_) => const NotificationsSettingPage()
+        );
+      case notificationsPage:
+        return MaterialPageRoute(
+          builder: (_) => const NotificationsPage()
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const TasksPage()
+        );
+    }
+  }
+}
---
+++ todo_app/lib/routes/detail/detail_task_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailTaskPage extends StatefulWidget {
+  const DetailTaskPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailTaskPage> createState() => _DetailTaskPageState();
+}
+
+class _DetailTaskPageState extends State<DetailTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
